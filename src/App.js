d1fb264cff8c359196a92bf067da4a326049ea4f
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-11 21:53:41 +0200

    9) React Router (Routes, Route and Link)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
-
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Normal components
 import NavBar from "./components/Nav";
+// Route components
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <NavBar />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
