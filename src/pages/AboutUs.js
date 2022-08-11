c32c838485924baa6878710d3e86e96ac0fa61af
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-11 15:28:32 +0200

    2) About Us component and page

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+// Page component is the acting container for above related components.
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
