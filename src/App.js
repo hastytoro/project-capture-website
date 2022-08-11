3bff2e68d22d3d4aaa04bf6bdce25a20ad950417
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-11 17:27:35 +0200

    8) Navbar component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 
+import NavBar from "./components/Nav";
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <NavBar />
       <AboutUs />
     </div>
   );
