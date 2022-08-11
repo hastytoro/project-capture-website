eec7ec2d359955e49345dc78b511131455a61bfa
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-11 16:29:17 +0200

    5) Introducing Styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+// Global style
+import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Capture</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import AboutSection from "../components/AboutSection";
 import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
