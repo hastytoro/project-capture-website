f30ce70ecf73abf7f49cebab01bcb7d23bec48e8
Luigi Lupini <luigi.lupini@gmail.com>
2022-08-12 12:18:56 +0200

    12) Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,54 @@
 import React from "react";
 import home1 from "../img/home1.png";
-
-// import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+// Framer motion imports
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  // Variants allow you to define animation states and organize them by name.
+  // They allow you to control animations throughout a component tree.
+  // And by switching a single `animate` prop.
+  // Parent variant:
+  const container = {
+    // from/start/enter
+    hidden: { x: 100 },
+    // to/end/leave
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.75,
+        ease: "easeOut",
+        staggerChildren: 0.75,
+      },
+    },
+  };
+
+  // Child variant inherits from parent and can staggerChildren:
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>Our work makes</h2>
+            <motion.h2 variants={titleAnim}>Our work makes</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography ideas that you have, we have
           professions with amazing skills.
